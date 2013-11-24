dedc2954a227b7c7733de11ac1f64c27cd2b2d4b
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2013-11-24 20:12:13 +1300

    [refactor] Adding comments for clarity.

--- response-handler-class.php
+++ response-handler-class.php
@@ -1,17 +1,21 @@
 <?php
 
-	define("DOC", 'doc');
-	define("DATA", 'data');
-	define("PROP", 'prop');
-	define("DEF", 'def');
+	# Standard data URI components
+	define("DOC", 'doc');		# document e.g. HTML return
+	define("DATA", 'data');		# data e.g. rdf, json, ttl
+	define("PROP", 'prop');		# property
+	define("DEF", 'def');		# definition
 
-	define("API", "api");
+	# Registry extension
+	define("API", "api");		# api functionality
 
+	# PRONOM API compatibility
 	define("PUID", "puid");
 	define("FMTPUID", "fmt");
 	define("XFMTPUID", "x-fmt");
 	define("XFMTPUIDALT", "xfmt");
 
+	# the-fr.org primary data objects
 	define("DATACLASS", "file-format");
 	define("FORMATREG", "format-registry");
 
@@ -22,7 +26,8 @@
 
 		function __construct()
 		{
-			$root = $_SERVER['DOCUMENT_ROOT'];
+			print $_SERVER['REQUEST_URI'];
+
 			$this->slugs_arr = get_slugs('/', $_SERVER['REQUEST_URI']);
 			$this->slugsize = sizeof($this->slugs_arr);
 		}
