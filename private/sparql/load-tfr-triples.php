f9d146a580d3f38c4d44b227ea5f69d6548f08e5
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2015-07-26 19:46:37 +1200

    [feature] For CRON.

--- private/sparql/load-tfr-triples.php
+++ private/sparql/load-tfr-triples.php
@@ -5,37 +5,42 @@
 	include_once("arc2access.php"); 
 	include_once("arc/ARC2.php");
 
-	/* MySQL and endpoint configuration */ 
-	$config = array(
-		/* db */
-		'db_host' => 'localhost', /* optional, default is localhost */
-		'db_name' => 'arc_db',
-		'db_user' => ARC2USER,
-		'db_pwd' => ARC2PASS,
+   function load_tfr_triples()
+   {
+	   /* MySQL and endpoint configuration */ 
+	   $config = array(
+		   /* db */
+		   'db_host' => 'localhost', /* optional, default is localhost */
+		   'db_name' => 'arc_db',
+		   'db_user' => ARC2USER,
+		   'db_pwd' => ARC2PASS,
 
-		/* store name */
-		'store_name' => 'tfr_db',
+		   /* store name */
+		   'store_name' => 'tfr_db',
 
-		/* stop after 100 errors */
-		'max_errors' => 100,
-	);
+		   /* stop after 100 errors */
+		   'max_errors' => 100,
+	   );
 
-	/* instantiation */
-	$ep = ARC2::getStore($config);
-	if (!$ep->isSetUp()) {
-		$ep->setUp(); /* create MySQL tables */
-	}
+	   /* instantiation */
+	   $ep = ARC2::getStore($config);
+	   if (!$ep->isSetUp()) {
+		   $ep->setUp(); /* create MySQL tables */
+	   }
 
-	$ep->reset();	# always loading a fresh set of triples
+	   $ep->reset();	# always loading a fresh set of triples
 
-	$load_triples = $ep->query('LOAD <../../public/tfr/tfr-triples/tfr-format-triples.nt>');
-	$load_ontology = $ep->query('LOAD <../../public/tfr/tfr-triples/tfr-ontology-triples.nt>');	
+	   $load_triples = $ep->query('LOAD <../../public/tfr/tfr-triples/tfr-format-triples.nt>');
+	   $load_ontology = $ep->query('LOAD <../../public/tfr/tfr-triples/tfr-ontology-triples.nt>');	
 
-	print "load triples result:" . "\r\n\r\n";	
-	print_r($load_triples);
+	   print "load triples result:" . "\r\n\r\n";	
+	   print_r($load_triples);
 
-	print "\r\n\r\n";
+	   print "\r\n\r\n";
 
-	print "load triples result:" . "\r\n\r\n";	
-	print_r($load_ontology);
+	   print "load triples result:" . "\r\n\r\n";	
+	   print_r($load_ontology);
+   }
+
+   load_tfr_triples();
 ?>
