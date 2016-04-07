659291b463a4bb812b362e58ff3d351d254f2000
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2016-04-07 20:14:49 +1200

    [feature] isDeprecated flag.

--- private/tfr-predicates.php
+++ private/tfr-predicates.php
@@ -8,11 +8,11 @@
 	define("TYPE_PREDICATE", "<http://the-fr.org/prop/format-registry/formatType>");
 	define("ALIAS_PREDICATE", "<http://www.w3.org/2004/02/skos/core#altLabel>");
 	define("EXTENSION_PREDICATE", "<http://the-fr.org/prop/format-registry/hasExtension>");
+   define("CONTAINER_PREDICATE", "<http://the-fr.org/prop/format-registry/hasPRONOMContainerMagic>");
+   define("BINARY_PREDICATE", "<http://the-fr.org/prop/format-registry/hasPRONOMBinaryMagic>");
+   define("DEPRECATED_PREDICATE", "<http://the-fr.org/prop/format-registry/isDeprecated>");
 
    // additional ontologies
    define("SAMEAS_PREDICATE", "<https://www.w3.org/2002/07/owl#sameAs>");
-   define("MAGIC_PREDICATE", "<http://digipres.org/formats/sources/pronom/formats/#hasMagic>");
-   define("CONTAINER_PREDICATE", "<http://the-fr.org/prop/format-registry/hasPRONOMContainerMagic>");
-   define("BINARY_PREDICATE", "<http://the-fr.org/prop/format-registry/hasPRONOMBinaryMagic>");
-   
+   define("MAGIC_PREDICATE", "<http://digipres.org/formats/sources/pronom/formats/#hasMagic>");   
 ?>
